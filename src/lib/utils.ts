9106516fedb1c228b3bb3723319664fbf590d73f
vitor <danielosilva@alu.ufc.br>
2025-09-25 15:27:26 -0300

    feat: add ClipboardCopy component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function copyToClipboard(value: string) {
+  await navigator.clipboard.writeText(value);
 }
