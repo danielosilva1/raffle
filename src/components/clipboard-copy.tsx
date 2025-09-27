bcff44de0949dc2af140647625f098978a169a14
vitor <danielosilva@alu.ufc.br>
2025-09-27 14:07:53 -0300

    feat: add toast to ClipboardCopy component

--- src/components/clipboard-copy.tsx
+++ src/components/clipboard-copy.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { copyToClipboard } from "@/lib/utils";
+import { toast } from "sonner";
 
 interface ClipboardCopyProps {
   value: string;
@@ -10,8 +11,14 @@ interface ClipboardCopyProps {
 export const ClipboardCopy = ({ value, children }: ClipboardCopyProps) => {
   return (
     <div
-      onClick={() => {
-        copyToClipboard(value);
+      onClick={async () => {
+        try {
+          await copyToClipboard(value);
+          toast.success("Copiado para a área de transferência");
+        } catch (error) {
+          console.error(error);
+          toast.error("Erro ao copiar para a área de transferência");
+        }
       }}
     >
       {children ? children : <Button variant="default">Copy</Button>}
